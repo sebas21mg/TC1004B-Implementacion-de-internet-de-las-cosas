eb935279c963c5de653ea20f4cf82af21b0a26ef
Sebastian Moncada <sebas.200221@gmail.com>
2022-11-24 12:44:12 -0600

    Agregar programa 3

---
+++ Códigos ESP32/Programa3-ConexionInternet.ino
@@ -0,0 +1,77 @@
+//Programa 3
+
+ 
+
+ 
+
+#include "ThingSpeak.h"
+#include "WiFi.h"
+
+#include "DHT.h"
+
+
+#define pin1 21       
+
+const char* ssid     = "Sebas' Redmi";
+const char* password = "sebas2103";
+
+unsigned long channelID = 1952324;               
+const char* WriteAPIKey = "DNRRF4MCSGC876JS";    
+
+WiFiClient cliente;
+
+DHT dht1(pin1, DHT11);    //El azul.
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Test de sensores:");
+
+  WiFi.begin(ssid,password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Wifi conectado!");
+
+  ThingSpeak.begin(cliente);
+
+  dht1.begin();
+}
+
+void loop() {
+
+  delay(1000);
+  leerdht1();
+
+
+  ThingSpeak.writeFields(channelID,WriteAPIKey);
+  Serial.println("Datos enviados a ThingSpeak!");
+  delay(14000);
+}
+
+void leerdht1() {
+  
+  float t1 = dht1.readTemperature();
+  float h1 = dht1.readHumidity();
+
+  while (isnan(t1) || isnan(h1)){
+    Serial.println("Lectura fallida en el sensor DHT11, repitiendo lectura...");
+    delay(1000);
+    t1 = dht1.readTemperature();
+    h1 = dht1.readHumidity();
+  }
+
+  Serial.print("Temperatura DHT11: ");
+  Serial.print(t1);
+  Serial.println(" ºC.");
+
+  Serial.print("Humedad DHT11: ");
+  Serial.print(h1);
+  Serial.println(" %."); 
+
+  Serial.println("-----------------------");
+
+  ThingSpeak.setField (1,t1);
+  ThingSpeak.setField (2,h1);
+}
